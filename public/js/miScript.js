e71c9cd7ba60e1225e0e2c233e609a7e5bb3cbde
Raul Estrada <raulestradaballesteros@gmail.com>
2016-11-06 12:41:37 +0100

    Changed layout of component to bring everything closer

--- public/js/miScript.js
+++ public/js/miScript.js
@@ -1,47 +1,40 @@
 $(document).ready(function() {
     var progressbar = $("#progressbar");
-      progressLabel = $(".progress-label");
-      $("#progressbar").progressbar({
-          value: true,
-          change: function() {
-            progressLabel.text(progressbar.progressbar("value") + "%" );
-          },
-          complete: function() {
-            progressLabel.text("Completado");
-          }
-      });
+    var progressLabel = $(".progress-label");
+    inicializarProgressBar();
+    inicializarContenedorDias();
+    inicializarFranjas();
 
-      var contenedorDias = $("#contenedorDias");
-      for (var i = 1; i <= 7; i++) {
-          var dia = new Date();
-          dia.setDate(dia.getDate() + i);
-          contenedorDias.append($("<label for=\"" + dia.getTime() + "\">"+ dia.toLocaleDateString() + "</label>"))
-          contenedorDias.append($("<input type=\"checkbox\" name=\"" + dia.getTime() + "\" id=\"" + dia.getTime() + "\">"));
-      }
-      var franjas = ["Mañana", "Tarde", "Noche"];
-      $("#contenedorDias input").on("change", function() {
-          if (this.checked) {
-              var contenedorFranja = $("#contenedorFranja");
-              var wrapperDia = $("<div id=\"" + this.id + "-wrapper\" class=\"controlgroup-horizontal\"><label class='first-inner-label'>" + this.labels[0].innerText + "</label></div>");
-              for (var franja in franjas) {
-                  wrapperDia.append($("<label for=\"" + this.id + franja + "\">"+ franjas[franja] + "</label>"))
-                  wrapperDia.append($("<input onchange='checkLimit(this)' type=\"checkbox\" name=\"" + this.id + franja + "\" id=\"" + this.id + franja + "\">"));
-              }
-              contenedorFranja.append(wrapperDia);
-              $("input").checkboxradio();
-          } else {
-              $("#" + this.id + "-wrapper").remove();
-          }
-      });
-
-      $("input").checkboxradio();
-      $(".controlgroup").controlgroup({"direction": "vertical"});
-      $(".controlgroup-horizontal").controlgroup({"direction": "horizontal"});
-      $("#dialog").dialog({
-          autoOpen: false,
-          modal: true
-      });
+    $("input").checkboxradio();
+    $(".controlgroup").controlgroup({"direction": "vertical"});
+    $(".controlgroup-horizontal").controlgroup({"direction": "horizontal"});
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true
+    });
 });
+
+function inicializarProgressBar() {
+    $("#progressbar").progressbar({
+        value: true,
+        change: function() {
+          progressLabel.text(progressbar.progressbar("value") + "%" );
+        },
+        complete: function() {
+          progressLabel.text("Completado");
+        }
+    });
+}
+
+function inicializarContenedorDias() {
+    var contenedorDias = $("#contenedorDias");
+    for (var i = 1; i <= 7; i++) {
+        var dia = new Date();
+        dia.setDate(dia.getDate() + i);
+        contenedorDias.append($("<label for=\"" + dia.getTime() + "\">"+ dia.toLocaleDateString() + "</label>"))
+        contenedorDias.append($("<input type=\"checkbox\" name=\"" + dia.getTime() + "\" id=\"" + dia.getTime() + "\">"));
+    }
+}
 
 function checkLimit(source) {
     if (source.checked) {
@@ -51,4 +44,22 @@ function checkLimit(source) {
             $(source).prop("checked", false);
         }
     }
+}
+
+function inicializarFranjas() {
+    var franjas = ["Mañana", "Tarde", "Noche"];
+    $("#contenedorDias input").on("change", function() {
+        if (this.checked) {
+            var contenedorFranja = $("#contenedorFranja");
+            var wrapperDia = $("<div id=\"" + this.id + "-wrapper\" class=\"controlgroup-horizontal\"><label class='first-inner-label col-xs-12 col-sm-2'>" + this.labels[0].innerText + "</label></div>");
+            for (var franja in franjas) {
+                wrapperDia.append($("<label for=\"" + this.id + franja + "\">"+ franjas[franja] + "</label>"))
+                wrapperDia.append($("<input onchange='checkLimit(this)' type=\"checkbox\" name=\"" + this.id + franja + "\" id=\"" + this.id + franja + "\">"));
+            }
+            contenedorFranja.append(wrapperDia);
+            $("input").checkboxradio();
+        } else {
+            $("#" + this.id + "-wrapper").remove();
+        }
+    });
 }
